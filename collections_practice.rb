86e1f43ae8168437baa6f4b01eec4f6b3d32e75b
Lessllie Real <rlessllie@yahoo.com>
2020-02-03 16:19:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  
+  
+  end 
+
+def sort_array_desc(array)
+  
+end
+
+def sort_array_char_count(array)
+  #returns a copy of the array with the strings ordered in ascending order by length
+end 
+
+def swap_elements(array, index, destination_index)
+  swap_elements_from_to([],2,1)
+end 
+
+def reverse_array(array)
+  array.reverse
+end 
+
+def kesha_maker(array)  
+  # replaces the third character in each string with a $ ("dollar sign")—Ke$ha style. Use the .each method to iterate and build a new array to return at the end of your method, just like we did in the "My Each" lab.
+end 
+
+def find_a 
+  #returns all the strings in the array passed to it that start_with? (hint) the letter "a". You'll want to use a high level iterator for this that finds, selects, or detects elements based on a condition.
+end 
+
+def sum_array
+  #adds together all of the integers in the array and returns their sum. .inject
+end 
+
+def add_s
+#adds an "s" to each word in the array except for the second element in the array ("feet" is already plural). each with index and collect 
+[1,2].each_with_index.collect{|element, index| }
+end
+
+
 
